6ddcd79e7941853ef2eb20781da2a62c23952eda
Joao Alex <44981295+joao-alex@users.noreply.github.com>
2019-11-05 11:33:46 -0300

    beginners

---
+++ 1847.py
@@ -0,0 +1,25 @@
+a, b, c = input().split()
+a = int(a)
+b = int(b)
+c = int(c)
+if(a > b):
+    if c > b:
+        print(':)')
+    else:
+        if(b-c < a-b):
+            print(':)')
+        else:
+            print(':(')
+elif(a < b):
+    if c < b:
+        print(':(')
+    else:
+        if(b-c > a-b):
+            print(':(')
+        else:
+            print(':)')
+else:
+    if c > a:
+        print(':)')
+    else:
+        print(':(')
