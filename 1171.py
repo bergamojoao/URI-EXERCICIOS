9fad67e8ccb6e068f07dc195618740d51fe3b7a4
Joao Alex <44981295+joao-alex@users.noreply.github.com>
2019-11-07 09:37:56 -0300

    dictiorary

---
+++ 1171.py
@@ -0,0 +1,15 @@
+n = int(input())
+array={}
+while n>0:
+    s = input()
+    if not s in array:
+        array[s]=1
+    else: array[s]+=1
+    n-=1
+aux = array.keys()
+chaves=[]
+for k in aux:
+    chaves.append(int(k))
+chaves.sort()
+for k in chaves:
+    print("%d aparece %d vez(es)"%(k,array[str(k)]))
